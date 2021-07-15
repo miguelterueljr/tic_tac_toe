ac60f46b88f09d6edf312e097c8fcef015fe63a9
Miguel Teruel <miguelterueljr@hotmail.com>
2021-07-15 10:11:21 -0300

    Estado inicial do jogo, declarado variaveis

---
+++ script.js
@@ -0,0 +1,17 @@
+let player_atual = 'X'
+let proximo_player = 'O'
+
+let playerXSelections = []
+let playerOSelections = []
+
+let winningCombinations = [ //variavel com as combinaçoes de vitoria
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+    [1, 4, 7]
+    [2, 5, 8]
+    [3, 6, 9]
+    [1, 5, 9]
+    [3, 5, 7]
+]
+
